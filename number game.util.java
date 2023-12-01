95718c12c4ff6ef5a3f102ede9250df620d4e395
RaillaDileep <150429348+RaillaDileep@users.noreply.github.com>
2023-12-01 18:31:12 +0530

    Update number game.util.java

--- number game.util.java
+++ number game.util.java
@@ -1,68 +1,43 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
-        // Additional details
         int lowerLimit = 1;
         int upperLimit = 100;
         int maxAttempts = 10;
         int roundsPlayed = 0;
         int totalAttempts = 0;
-
         boolean playAgain = true;
-
         while (playAgain) {
-            // Step 1: Generate a random number within the specified range
-            int secretNumber = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
-
-            System.out.println("Guess the number between " + lowerLimit + " and " + upperLimit);
-
-            int attempts = 0;
+            int secretNumber = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit; System.out.println("Guess the number between " + lowerLimit + " and " + upperLimit);int attempts = 0;
             boolean guessedCorrectly = false;
-
             while (attempts < maxAttempts) {
-                // Step 2: Prompt the user to enter their guess
                 System.out.print("Enter your guess: ");
                 int userGuess = scanner.nextInt();
-
-                // Step 3: Compare the user's guess and provide feedback
                 if (userGuess == secretNumber) {
                     System.out.println("Congratulations! You guessed the correct number " + secretNumber + "!");
                     guessedCorrectly = true;
-                    totalAttempts += attempts + 1;  // Calculate score
+                    totalAttempts += attempts + 1;
                     break;
                 } else if (userGuess < secretNumber) {
                     System.out.println("Too low! Try again.");
                 } else {
                     System.out.println("Too high! Try again.");
                 }
-
                 attempts++;
             }
-
-            // Check if the user has run out of attempts
             if (!guessedCorrectly) {
                 System.out.println("Sorry, you've run out of attempts. The correct number was " + secretNumber + ".");
             }
-
-            // Additional details
             roundsPlayed++;
-
-            // Step 6: Ask if the user wants to play again
             System.out.print("Do you want to play again? (yes/no): ");
             String playAgainInput = scanner.next().toLowerCase();
             playAgain = playAgainInput.equals("yes");
         }
-
-        // Step 7: Display the user's score
         System.out.println("Rounds played: " + roundsPlayed);
         System.out.println("Total attempts: " + totalAttempts);
-
-        // Close the scanner
         scanner.close();
     }
 }
